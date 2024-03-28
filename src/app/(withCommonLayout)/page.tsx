a3ef10dcc1e11c6dd708357a08e01f56bca50964
$apucsd <apusutradhar77@gmail.com>
2024-03-28 13:03:46 +0600

    added scroll to top

--- src/app/(withCommonLayout)/page.tsx
+++ src/app/(withCommonLayout)/page.tsx
@@ -2,6 +2,7 @@ import Banner from "@/components/home/Banner";
 import BooksCategory from "@/components/home/BooksCategory";
 import FeaturedBooks from "@/components/home/FeaturedBooks";
 import NewBooks from "@/components/home/NewBooks";
+import ScrollToTop from "@/components/home/ScrollToTop";
 import Slider from "@/components/home/Slider";
 import TopRatedBooks from "@/components/home/TopRated";
 import { getBooksByQuery } from "@/services/bookActions/getBookByQuery";
@@ -16,6 +17,7 @@ export default async function Home() {
       <NewBooks newBooks={newBooks} />
       <TopRatedBooks />
       <Banner />
+      <ScrollToTop />
     </div>
   );
 }
---
+++ src/components/home/ScrollToTop.tsx
@@ -0,0 +1,57 @@
+"use client";
+import { useState, useEffect } from "react";
+import { ChevronUp } from "lucide-react";
+import { AnimatePresence, motion } from "framer-motion";
+import { Button } from "../ui/button";
+
+const ScrollToTop = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  useEffect(() => {
+    const toggleVisibility = () => {
+      if (window.scrollY > 50) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
+    };
+
+    window.addEventListener("scroll", toggleVisibility);
+    return () => {
+      window.removeEventListener("scroll", toggleVisibility);
+    };
+  }, []);
+
+  return (
+    <AnimatePresence>
+      {isVisible && (
+        <motion.div
+          key="scrollToTop"
+          initial={{ opacity: 0, x: 50 }}
+          animate={{ opacity: 1, x: 0 }}
+          exit={{ opacity: 0, x: 20 }}
+          transition={{ duration: 0.2 }} // Set duration here
+          style={{
+            position: "fixed",
+            bottom: "50px",
+            right: "50px",
+            zIndex: 9999,
+          }}
+        >
+          <Button className={`px-2 py-3 rounded-full`} onClick={scrollToTop}>
+            <ChevronUp className="size-6" />
+          </Button>
+        </motion.div>
+      )}
+    </AnimatePresence>
+  );
+};
+
+export default ScrollToTop;
