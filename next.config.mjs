0f8fdb4d2d3e5f672b1c9ccb614579691fc48bc2
$apucsd <apusutradhar77@gmail.com>
2024-04-02 20:58:37 +0600

    added sound when add to cart using webpack loader

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,15 @@ const nextConfig = {
       },
     ],
   },
+  webpack(config, options) {
+    config.module.rules.push({
+      test: /\.mp3$/,
+      use: {
+        loader: "file-loader",
+      },
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
--- src/components/ui/AddToCartButton.tsx
+++ src/components/ui/AddToCartButton.tsx
@@ -4,8 +4,11 @@ import { Button } from "./button";
 import { addToCart } from "@/redux/features/cartSlice";
 import { ShoppingCart } from "lucide-react";
 import { toast } from "sonner";
+// import addToCartSound from "@/assets/sound/pick.mp3";
 const AddToCartButton = ({ _id, title, image, price, authors }: any) => {
+  const audio = new Audio("/pick.mp3");
   const handleAddToCart = () => {
+    audio.play();
     dispatch(addToCart({ _id, title, image, price, authors }));
     toast.success("This book added to your cart!!!", { duration: 1000 });
   };
